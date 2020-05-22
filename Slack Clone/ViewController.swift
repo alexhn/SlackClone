e2f38bf6b9315daf603cd8f21c7ee3724246ccf6
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-23 01:54:31 +0300

    Initial Commit

---
+++ Slack Clone/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Slack Clone
+//
+//  Created by Aleksandr Nikiforov on 23.05.2020.
+//  Copyright © 2020 Aleksandr Nikiforov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
