695066dc86caa6e0a0dd06bd9377c79c43069e6f
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-23 02:05:21 +0300

    Rename view controller

--- Slack Clone/LoginViewController.swift
+++ Slack Clone/LoginViewController.swift
@@ -8,12 +8,10 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class LoginViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
