97cf90b49f9f36615f6580e3931fb72b1b731d0c
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-25 17:32:02 +0300

    Load list of titles

--- Slack Clone/ChannelsViewController.swift
+++ Slack Clone/ChannelsViewController.swift
@@ -9,23 +9,30 @@
 import Cocoa
 import Parse
 
-class ChannelsViewController: NSViewController {
+class ChannelsViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
 
     @IBOutlet weak var nameLabel: NSTextField!
     @IBOutlet weak var profilePicImageView: NSImageView!
     
     var addChannelWindowController : NSWindowController?
     
+    @IBOutlet weak var channelsTableView: NSTableView!
+    
+    var channels : [PFObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        channelsTableView.dataSource = self
+        channelsTableView.delegate = self
     }
     
     override func viewDidAppear() {
+        getChannels()
         if let user = PFUser.current() {
            if let name = user["name"] as? String {
                nameLabel.stringValue = name
            }
-            if let imageFile = user["profilePic"] as? PFFileObject {
+           if let imageFile = user["profilePic"] as? PFFileObject {
                 imageFile.getDataInBackground { (data: Data?, error: Error?) in
                     if error == nil {
                         if data != nil {
@@ -52,4 +59,29 @@ class ChannelsViewController: NSViewController {
         addChannelWindowController?.showWindow(nil)
     }
     
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return channels.count
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        let channel = channels[row]
+        if let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "channelCell"), owner: nil) as? NSTableCellView {
+            if let title = channel["title"] as? String {
+                cell.textField?.stringValue = "#\(title)"
+            }
+            return cell
+        }
+        return nil
+    }
+    
+    func getChannels() {
+        let query = PFQuery(className: "Channel")
+        query.order(byAscending: "title")
+        query.findObjectsInBackground { (channels: [PFObject]?, error: Error?) in
+            if channels != nil {
+                self.channels = channels!
+                self.channelsTableView.reloadData()
+            }
+        }
+    }
 }
