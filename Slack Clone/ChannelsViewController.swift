b9183c13ad9ffefd14b77466e874d3e64e177ff2
Aleksandr Nikiforov <alex@alexn.ee>
2020-05-25 16:19:51 +0300

    Load user image and name after log in

---
+++ Slack Clone/ChannelsViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ChannelsViewController.swift
+//  Slack Clone
+//
+//  Created by Aleksandr Nikiforov on 25.05.2020.
+//  Copyright © 2020 Aleksandr Nikiforov. All rights reserved.
+//
+
+import Cocoa
+import Parse
+
+class ChannelsViewController: NSViewController {
+
+    @IBOutlet weak var nameLabel: NSTextField!
+    @IBOutlet weak var profilePicImageView: NSImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear() {
+        if let user = PFUser.current() {
+           if let name = user["name"] as? String {
+               nameLabel.stringValue = name
+           }
+            if let imageFile = user["profilePic"] as? PFFileObject {
+                imageFile.getDataInBackground { (data: Data?, error: Error?) in
+                    if error == nil {
+                        if data != nil {
+                            let image = NSImage(data: data!)
+                            self.profilePicImageView.image = image
+                        }
+                    } else {
+                        print("Problem downloading user image.")
+                    }
+                }
+            }
+        }
+    }
+    
+    @IBAction func logoutButtonClicked(_ sender: Any) {
+        PFUser.logOut()
+        if let mainWindowController = view.window?.windowController as? MainWindowController {
+           mainWindowController.moveToLogin()
+        }
+    }
+    
+}
